6befc260f80c72ca24b5b7e129341fe3294a7e85
CeeF1 <moskalakamil07@gmail.com>
2022-11-08 00:17:38 +0100

    wise word section <3

---
+++ components/atoms/WiseWords/WiseWordsIllustration.vue
@@ -0,0 +1,12 @@
+<template>
+        <div>
+            <img src="/images/wiseWords/illustrations/1.svg" />
+        </div>
+</template>
+<style lang="scss" scoped>
+    div{
+        position: absolute;
+        right: 5vw;
+        top: 100vh;
+    }
+</style>
